9c910b4ee43c600b06b08489b75bcdc67e139c73
M-Darab <m.darabkhan@codedistrict.com>
2023-02-21 11:08:14 +0500

    FaceDetectionBlur

--- FaceDetection.py
+++ FaceDetection.py
@@ -5,29 +5,28 @@ import numpy as np
 video = cv2.VideoCapture(2)
 
 faceCascade = cv2.CascadeClassifier("harcascades/haarcascade_frontalface_default.xml")
-eyeCascade = cv2.CascadeClassifier("harcascades/haarcascade_eye.xml")
 
 if video.isOpened() == False:
     print("Error")
 
 while True:
     ret, frame = video.read()
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     faces = faceCascade.detectMultiScale(
-        grayFrame, scaleFactor=1.2, minNeighbors=5, minSize=(25, 25)
-    )
-    eyes = eyeCascade.detectMultiScale(
-        grayFrame, scaleFactor=1.2, minNeighbors=5, minSize=(25, 25)
+        frame, scaleFactor=1.05, minNeighbors=5, minSize=(25, 25)
     )
 
     for x, y, w, h in faces:
-        cv2.rectangle(grayFrame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        center = np.array(((x + (x + w)) / 2, (y + (y + h)) / 2)).astype(int)
 
-    for xe, ye, we, he in eyes:
-        cv2.rectangle(grayFrame, (xe, ye), (xe + we, ye + he), (0, 255, 0), 2)
+        mask = np.zeros(shape=frame.shape, dtype=np.uint8)
+        frameBlured = cv2.blur(src=frame, ksize=(25, 25), borderType=cv2.BORDER_DEFAULT)
+    
+        mask = cv2.circle(mask, center, 130, (255, 255, 255), -1)
+        frame = np.where(mask != np.array([255, 255, 255]), frame, frameBlured)
 
     if ret == True:
-        cv2.imshow("Video", grayFrame)
+        cv2.imshow("Video", frame)
 
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
